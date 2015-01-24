08a51a293300eb20fd8f45553b4e2f825ea1462e
Suftin, Ivan <isuftin@usgs.gov>
2015-01-23 21:03:18 -0600

    Updated version and readme and license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cida_consul_client'
 maintainer       'Ivan Suftin'
 maintainer_email 'isuftin@usgs.gov'
-license          'gplv3'
+license          'Public Domain'
 description      'Role cookbook for the Consul client install'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
